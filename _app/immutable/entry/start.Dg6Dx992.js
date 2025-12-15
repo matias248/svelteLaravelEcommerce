253aa642470b6e4153187b9090531a6ec0272fdc
matias248 <matias.andorra@gmail.com>
2025-12-15 21:37:10 +0100

    Updates

--- _app/immutable/entry/start.Dg6Dx992.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/DlzR5_pz.js";export{o as load_css,r as start};
---
+++ _app/immutable/nodes/12.C9caPh9W.js
@@ -0,0 +1,1 @@
+import"../chunks/BmDDdK99.js";import"../chunks/BQwqAryc.js";import{S as o}from"../chunks/_g7IPn97.js";function n(e){o(e,{id:void 0,title:"Create a new store"})}export{n as component};
