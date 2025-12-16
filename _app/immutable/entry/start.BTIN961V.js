c221530e4b62a4740d2c2001ccd876410322b302
matias248 <matias.andorra@gmail.com>
2025-12-16 13:01:17 +0100

    Updates

--- _app/immutable/entry/start.BTIN961V.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/CF1aXvwk.js";export{o as load_css,r as start};
---
+++ _app/immutable/entry/start.DeJ0Wo4h.js
@@ -0,0 +1,1 @@
+import{l as o,a as r}from"../chunks/D69ezIGz.js";export{o as load_css,r as start};
